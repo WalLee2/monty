39e4507a2d7258dae092a822d6033f20d7d8ab15
Walton Lee <mr.waltonlee@me.com>
2017-01-30 02:12:14 +0000

    fixing errors

--- character_num_check.c
+++ character_num_check.c
@@ -2,10 +2,10 @@
 /**
  *Chk - Checks to see if the input made by the user is an int or not
  *@out: The next character in the same string of the user input
- *@l_n: Line number of the user input being evaluated.
+ *@l_num: Line number of the user input being evaluated.
  *Return: 0 on success, 1 on failure/error
  */
-int Chk(const char *out, unsigned int l_n)
+int Chk(const char *out, unsigned int l_num)
 {
 	int i, check;
 
@@ -16,7 +16,7 @@ int Chk(const char *out, unsigned int l_n)
 	{
 		if (out[i] < '0' || out[i] > '9')
 		{
-			printf("L%d: usage: push integer\n", l_n);
+			printf("L%d: usage: push integer\n", l_num);
 			errorHandling = 1;
 			return (1);
 		}
